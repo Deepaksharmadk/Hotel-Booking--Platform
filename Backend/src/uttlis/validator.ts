724b828cfdf581082779cdf8aaad6b6853f7c16a
deepaksharmadk <dks847203@gmail.com>
2024-04-07 15:39:15 +0530

    added validator

---
+++ Backend/src/uttlis/validator.ts
@@ -0,0 +1,10 @@
+import { check, validationResult } from "express-validator";
+let validator = [
+  check("firstName", "First Name is required").isString(),
+  check("lastName", "Last Name is required").isString(),
+  check("email", "Email is required").isEmail(),
+  check("password", "Password with 6 or more characters required").isLength({
+    min: 6,
+  }),
+];
+export default validator;
