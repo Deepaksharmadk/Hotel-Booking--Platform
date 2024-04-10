a4e927e24800c108b4ab9afdf4b434cc6b8c30b0
deepaksharmadk <dks847203@gmail.com>
2024-04-10 22:23:24 +0530

    added user logicand fix bug

--- frontend/src/apiClient.ts
+++ frontend/src/apiClient.ts
@@ -1,6 +1,6 @@
+import toast from "react-hot-toast";
 import { RegisterFormData } from "./pages/Register";
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "";
-console.log("hello", API_BASE_URL);
 
 export const register = async (formData: RegisterFormData) => {
   const response = await fetch(`${API_BASE_URL}/api/auth/register`, {
@@ -11,7 +11,10 @@ export const register = async (formData: RegisterFormData) => {
     },
     body: JSON.stringify(formData),
   });
+  console.log(formData);
   const responseBody = await response.json();
+
+  toast.success(responseBody.message);
   if (!response.ok) {
     throw new Error(responseBody.message);
   }
