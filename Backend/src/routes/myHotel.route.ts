e866089201327d36eeeaf4744204a6c2cf34fa2e
deepaksharmadk <dks847203@gmail.com>
2024-04-12 03:22:58 +0530

    added hostel route

---
+++ Backend/src/routes/myHotel.route.ts
@@ -0,0 +1,10 @@
+import express, { Request, Response } from "express";
+import { hotelValidation } from "../uttlis/validator";
+import { myHotel } from "../controller/myhotel.controller";
+import verifyToken from "../middleare/auth";
+import { upload } from "../uttlis/multer";
+const router = express.Router();
+router
+  .route("/")
+  .post(verifyToken, hotelValidation, upload.array("imageFiles", 6), myHotel);
+export default router;
