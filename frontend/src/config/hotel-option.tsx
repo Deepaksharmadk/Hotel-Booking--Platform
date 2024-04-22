8114389135a6fe8e1ae0ebe122a468c9479508b2
deepaksharmadk <dks847203@gmail.com>
2024-04-22 19:08:31 +0530

    fix bug

--- frontend/src/config/hotel-option.tsx
+++ frontend/src/config/hotel-option.tsx
@@ -26,3 +26,21 @@ export const hotelFacilities = [
   "Spa",
   "Fitness Center",
 ];
+export type HotelType = {
+  key?: string;
+  _id: string;
+  userId: string;
+  name: string;
+  city: string;
+  country: string;
+  description: string;
+  type: string;
+  adultCount: number;
+  childCount: number;
+  facilities: string[];
+  pricePerNight: number;
+  starRating: number;
+  imageUrls: string[];
+  lastUpdated: Date;
+  // bookings: BookingType[];
+};
