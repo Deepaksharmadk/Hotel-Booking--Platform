31505ea50a6f22c26f3bbbf88a45bf6cd55bf806
deepaksharmadk <dks847203@gmail.com>
2024-05-04 17:41:32 +0530

    added url whitelist

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -5,9 +5,15 @@ import "dotenv/config";
 import cookieParser from "cookie-parser";
 import path from "path";
 import { v2 as cloudinary } from "cloudinary";
-mongoose.connect(process.env.MONGO_URL as string).then(() => {
-  console.log("database connected");
-});
+mongoose
+  .connect(process.env.MONGO_URL as string)
+  .then(() => {
+    console.log("database connected");
+  })
+  .catch((err) => {
+    console.log(err);
+    process.exit(1);
+  });
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
 
@@ -21,11 +27,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
+console.log(process.env.FRONTEND_URL);
 
 import authRoutes from "./routes/auth.route";
 import myHotelRoutes from "./routes/myHotel.route";
@@ -36,5 +43,5 @@ app.use("/api/users", userRoutes);
 app.use("/api/my-hotels", myHotelRoutes);
 
 app.listen(process.env.PORT, () => {
-  console.log("server running on port 7000");
+  console.log(`server running on port ${process.env.PORT}`);
 });
