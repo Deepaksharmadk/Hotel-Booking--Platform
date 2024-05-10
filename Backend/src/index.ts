a37e0c666c82e21d6dda52a2c2388f6df3bfe42e
deepaksharmadk <dks847203@gmail.com>
2024-05-10 16:14:31 +0530

    added url

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "https://guestrest.vercel.app",
+    origin: ["https://guestrest.vercel.app", "https://guestbackend.vercel.app"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
